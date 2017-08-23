ecfce9b06ebebc81be197f9450c09cf2b2f9f44e
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-23 20:38:35 +1000

    Fin!

--- First Dabblings.rb
+++ First Dabblings.rb
@@ -1,15 +1,19 @@
 print "What's your first name?"
 first_name = gets.chomp
+first_name.capitalize!
 puts "Your name is #{first_name}!"
 
 print "What's your last name?"
 last_name = gets.chomp
+last_name.capitalize!
 puts "Your last name is #{last_name}!"
 
 print "What city are you from?"
 city = gets.chomp
+city.capitalize!
 puts "You live in #{city}!"
 
 print "What state are you from? (Use an abbreviation!)"
 state = gets.chomp
+state.upcase!
 puts "#{city} is located in #{state}!"
