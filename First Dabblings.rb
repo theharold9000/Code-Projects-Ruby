7ba718f710c76fcaab13b702c33df483cb37fd19
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-23 20:35:27 +1000

    Putting stuff in!

--- First Dabblings.rb
+++ First Dabblings.rb
@@ -1,11 +1,15 @@
 print "What's your first name?"
 first_name = gets.chomp
+puts "Your name is #{first_name}!"
 
 print "What's your last name?"
 last_name = gets.chomp
+puts "Your last name is #{last_name}!"
 
 print "What city are you from?"
 city = gets.chomp
+puts "You live in #{city}!"
 
 print "What state are you from? (Use an abbreviation!)"
 state = gets.chomp
+puts "#{city} is located in #{state}!"
